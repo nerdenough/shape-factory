7d08d499494e3885a4d5bfd671b2becb0d61a3b7
Brendan Goodenough <brendan@goodenough.nz>
2016-04-06 23:14:33 +1200

    Add shape image assets

--- public/js/preloader.js
+++ public/js/preloader.js
@@ -9,6 +9,10 @@ Game.Preloader.prototype.create = function () {
 
   loadText = game.add.text(32, 32, 'Loading', {fill: '#ffffff'});
 
+  game.load.image('square', 'assets/square.png');
+  game.load.image('circle', 'assets/circle.png');
+  game.load.image('triangle', 'assets/triangle.png');
+
   game.load.start();
 };
 
