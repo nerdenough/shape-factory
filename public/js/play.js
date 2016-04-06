ee86bd46c475d0200a00b31819f78e7200225f9a
Brendan Goodenough <brendan@goodenough.nz>
2016-04-06 23:58:41 +1200

    Recreate shape when it goes out of bounds

--- public/js/play.js
+++ public/js/play.js
@@ -1,23 +1,37 @@
 Game.Play = function () {};
 
-var controls;
+var velX;
+var spacebar;
 var shape;
+var released;
 
 Game.Play.prototype.create = function () {
-  game.physics.startSystem(Phaser.Physics.ARCADE);
-
+  velX = 200;
   chooseShape();
 
-  controls = {
-    left: this.input.keyboard.addKey(Phaser.Keyboard.A),
-    right: this.input.keyboard.addKey(Phaser.Keyboard.D),
-    spacebar: this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
-  };
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  spacebar = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 };
 
 Game.Play.prototype.update = function () {
-  if (controls.spacebar.isDown) {
-    shape.body.velocity.y = 350;
+  if (!released) {
+    if (shape.body.x < 0) {
+      shape.body.velocity.x = velX;
+    }
+
+    if (shape.body.x + shape.body.width >= game.width) {
+      shape.body.velocity.x = -velX;
+    }
+  }
+
+  if (spacebar.isDown) {
+    released = true;
+    shape.body.gravity.y = 1000;
+  }
+
+  if (!shape.alive) {
+    released = false;
+    chooseShape();
   }
 };
 
@@ -26,6 +40,8 @@ function chooseShape() {
 
   shape = game.add.sprite(game.world.centerX, 100, 'shape-' + i);
   shape.anchor.setTo(0.5);
-
   game.physics.enable(shape);
+  shape.body.velocity.x = velX;
+  shape.checkWorldBounds = true;
+  shape.outOfBoundsKill = true;
 }
